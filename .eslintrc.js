54ebca93a3d3c0d28923afa1cd69fccdb1411065
Nikita Schastnyy <n.schastnyy@andersenlab.com>
2021-07-28 17:27:25 +0300

    fixup! config: add eslint + prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,25 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
+    ecmaVersion: 2020,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint',
+    '@typescript-eslint/eslint-plugin',
+    'prettier',
+    'nestjs',
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'prettier',
     'plugin:prettier/recommended',
+    'plugin:nestjs/recommended',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
+    es6: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
@@ -20,5 +29,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'lf',
+      },
+    ],
+    'no-debugger': 'off',
+    'no-console': 'warn',
+    'linebreak-style': ['error', 'unix'],
   },
 };
